7038203ceab26a7dc2ca3ca861e8e717b40271bd
Roger Issonaga <roger.issonaga@gmail.com>
2022-12-10 19:39:43 -0300

    updt capturando-dados-com-selenium

--- capturando-dados-com-selenium/main.py
+++ capturando-dados-com-selenium/main.py
@@ -1,5 +1,12 @@
-
+#%%
 import selenium
 from selenium import webdriver
 
-driver = webdriver.Chrome('./src/chromedriver')
+#%%
+driver = webdriver.Chrome('/home/roger/How/EngDados/how-engdados/capturando-dados-com-selenium/src/chromedriver')
+
+#%%
+driver.get('https://buscacepinter.correios.com.br/app/endereco/index.php?t')
+elem_cep = driver.find_element("name", "endereco")
+
+#%%/src/chromedriver
